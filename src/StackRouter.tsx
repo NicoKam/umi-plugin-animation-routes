bd913388ff1b842df3f11b879f2b552a2230f11f
NicoKam <331677620@qq.com>
2020-12-22 16:29:44 +0800

    update - add animtion on go

--- src/StackRouter.tsx
+++ src/StackRouter.tsx
@@ -18,31 +18,51 @@ const StackRouter: React.FC<StackRouterProps> = (props) => {
   const { history, staticContext, match } = context;
   const [historyStack, offsetIndex, { push, replace, pop, go }] = useHistoryStack(history);
   const _this = useRef({
+    // 主动调用go方法时记录go的长度
     goStep: 0,
+    // 是否使用动画(临时)
+    useAnimation: true,
     lastHistoryKey: history.location.state?._historyKey ?? 0,
     lastLastHistoryKey: history.location.state?._historyKey ?? 0,
   }).current;
 
   useEffect(() => {
-    injectHistory(history, (goStep) => {
-      _this.goStep = goStep;
+    injectHistory(history, {
+      onGoStep: (goStep, options) => {
+        const { animation } = options;
+        if (animation === false) _this.useAnimation = false;
+        _this.goStep = goStep;
+      },
     });
     return history.listen((newLocation: any, _action: string) => {
+      console.log('changed', newLocation);
       let action = _action;
+
+      const curKey = newLocation.state?._historyKey ?? 0;
+
+      // console.log(_this.lastHistoryKey, curKey);
+
+      /* fix: 浏览器前进后退时，action总是为'POP'， */
+      if (_this.lastHistoryKey !== curKey && action === 'POP') {
+        if (curKey > _this.lastHistoryKey) {
+          /* 如果key增加了，说明时进行了前进操作 */
+          action = 'PUSH';
+        }
+      }
+      if (curKey === _this.lastHistoryKey) {
+        action = 'REPLACE';
+      }
+      _this.lastHistoryKey = curKey;
+      if (_this.useAnimation === false) {
+        _this.lastLastHistoryKey = _this.lastHistoryKey;
+        _this.useAnimation = true;
+      }
+
       if (_this.goStep) {
+        /* 有明确的前进后退标识 */
         go(_this.goStep);
         _this.goStep = 0;
       } else {
-        const curKey = newLocation.state?._historyKey ?? 0;
-
-        /* fix: 浏览器前进后退时，action总是为'POP'， */
-        if (_this.lastHistoryKey !== curKey && action === 'POP') {
-          if (curKey > _this.lastHistoryKey) {
-            /* 如果key增加了，说明时进行了前进操作 */
-            action = 'PUSH';
-          }
-        }
-        _this.lastHistoryKey = curKey;
         switch (action) {
           case 'PUSH':
             push();
@@ -60,7 +80,14 @@ const StackRouter: React.FC<StackRouterProps> = (props) => {
     });
   }, [history]);
 
-  const classNames = `stack-routes-anim-${_this.lastLastHistoryKey - _this.lastHistoryKey < 0 ? 'push' : 'pop'}`;
+  // console.log(_this.lastLastHistoryKey, _this.lastHistoryKey);
+
+  const classNames = `stack-routes-anim-${(() => {
+    const keyChanged = _this.lastLastHistoryKey - _this.lastHistoryKey;
+    if (keyChanged < 0) return 'push';
+    if (keyChanged > 0) return 'pop';
+    return 'no-changed';
+  })()}`;
   _this.lastLastHistoryKey = _this.lastHistoryKey;
   return (
     <TransitionGroup childFactory={child => React.cloneElement(child, { classNames })}>
